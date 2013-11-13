8610f08d2d15dd3dd91fd5efdb621969fb3e13e9
David Sulc <davidsulc@gmail.com>
2013-11-13 18:42:01 +0100

    deal with legacy APIs

--- assets/js/apps/contacts/edit/edit_view.js
+++ assets/js/apps/contacts/edit/edit_view.js
@@ -1,7 +1,7 @@
 ContactManager.module("ContactsApp.Edit", function(Edit, ContactManager, Backbone, Marionette, $, _){
   Edit.Contact = ContactManager.ContactsApp.Common.Views.Form.extend({
     initialize: function(){
-      this.title = "Edit " + this.model.get("firstName") + " " + this.model.get("lastName");
+      this.title = "Edit " + this.model.get("fullName");
     },
 
     onRender: function(){
--- assets/js/entities/contact.js
+++ assets/js/entities/contact.js
@@ -1,11 +1,27 @@
 ContactManager.module("Entities", function(Entities, ContactManager, Backbone, Marionette, $, _){
   Entities.Contact = Backbone.Model.extend({
-    urlRoot: "contacts",
+    urlRoot: "contacts_legacy",
 
     defaults: {
       firstName: "",
       lastName: "",
       phoneNumber: ""
+    },
+
+    parse: function(response){
+      var data = response;
+      if(response && response.contact){
+        data = response.contact;
+      }
+      data.fullName = data.firstName + " ";
+      data.fullName += data.lastName;
+      return data;
+    },
+
+    toJSON: function(){
+      return {
+        data: _.clone(this.attributes)
+      };
     },
 
     validate: function(attrs, options) {
@@ -28,7 +44,7 @@ ContactManager.module("Entities", function(Entities, ContactManager, Backbone, M
   });
 
   Entities.ContactCollection = Backbone.Collection.extend({
-    url: "contacts",
+    url: "contacts_legacy",
     model: Entities.Contact,
     comparator: "firstName"
   });
