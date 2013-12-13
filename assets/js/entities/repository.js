90d7032195d2800dea9731ee5cb225031bc0fa02
David Sulc <davidsulc@gmail.com>
2013-12-13 17:58:45 +0100

    wrapping GitHub's API: deleting repos

--- assets/js/entities/repository.js
+++ assets/js/entities/repository.js
@@ -13,34 +13,40 @@ ContactManager.module("Entities", function(Entities, ContactManager, Backbone, M
     sync: function(method, model, options){
       var username = "USERNAME",
           password = "PASSWORD",
-          baseUrl = "https://api.github.com";
-
-      switch(method){
-        case "create":
+          baseUrl = "https://api.github.com",
           config = {
             beforeSend: function (xhr) {
               xhr.setRequestHeader ("Authorization", "Basic " + btoa(username + ":" + password));
-            },
+            }
+          };
+
+      switch(method){
+        case "create":
+          config = _.extend(config, {
             method: "POST",
             url: baseUrl + "/user/repos",
             data: JSON.stringify(model.pick("name", "description", "homepage",
               "private", "has_issues", "has_wiki", "has_downloads", "team_id",
               "auto_init", "gitignore_template"
             ))
-          };
+          });
           break;
 
         case "read":
-          config = {
+          config = _.extend(config, {
             method: "GET",
             url: baseUrl + "/repos/" + username + "/" + model.get("name")
-          };
+          });
           break;
 
         case "update":
           break;
 
         case "delete":
+          config = _.extend(config, {
+            method: "DELETE",
+            url:  baseUrl + "/repos/" + username + "/" + model.get("name")
+          });
           break;
       };
 
