4cd69fb7d733cf386d3c3552314525aa2dbc0df3
David Sulc <davidsulc@gmail.com>
2014-04-08 21:10:46 +0200

    manage a contact's acquaintances

--- assets/js/apps/contacts/show/show_controller.js
+++ assets/js/apps/contacts/show/show_controller.js
@@ -10,15 +10,68 @@ ContactManager.module("ContactsApp.Show", function(Show, ContactManager, Backbon
         }
       });
       $.when(fetchingContact).done(function(contact){
-        var contactView = new Show.Contact({
-          model: contact
-        });
+        var acquaintancesFetched = contact.get("acquaintances").fetch(),
+            strangersFetched = contact.get("strangers").fetch();
+        $.when(acquaintancesFetched, strangersFetched).done(function(){
+          var contactView = new Show.Contact({
+            model: contact
+          });
 
-        Show.Controller.listenTo(contactView, "contact:edit", function(contact){
-          ContactManager.trigger("contact:edit", contact.get("id"));
-        });
+          Show.Controller.listenTo(contactView, "show", function(){
+            var acquaintances = contact.get("acquaintances");
+            var strangers = contact.get("strangers");
 
-        ContactManager.regions.main.show(contactView);
+            var acquaintancesUrl = _.result(contact, "url") + "/acquaintances/";
+            contact.listenTo(acquaintances, "add", function(model){
+              $.ajax({
+                url: acquaintancesUrl + model.get("id"),
+                type: "POST",
+                dataType: "json",
+                success: function(){
+                  contact.get("strangers").remove(model);
+                }
+              });
+            });
+            contact.listenTo(acquaintances, "remove", function(model){
+              $.ajax({
+                url: acquaintancesUrl + model.get("id"),
+                type: "DELETE",
+                dataType: "json",
+                success: function(){
+                  contact.get("strangers").add(model);
+                }
+              });
+            });
+
+            var acquaintancesView = new ContactManager.Common.Views.PaginatedView({
+              collection: acquaintances,
+              mainView: Show.Acquaintances,
+              propagatedEvents: ["childview:acquaintance:remove"]
+            });
+
+            var strangersView = new ContactManager.Common.Views.PaginatedView({
+              collection: strangers,
+              mainView: Show.Strangers,
+              propagatedEvents: ["childview:acquaintance:add"]
+            });
+
+            Show.Controller.listenTo(acquaintancesView, "childview:acquaintance:remove", function(view, args){
+              contact.get("acquaintances").remove(args.model);
+            });
+            Show.Controller.listenTo(strangersView, "childview:acquaintance:add", function(view, args){
+              contact.get("acquaintances").add(args.model);
+            });
+
+            contactView.acquaintancesRegion.show(acquaintancesView);
+            contactView.strangersRegion.show(strangersView);
+          });
+
+          Show.Controller.listenTo(contactView, "contact:edit", function(contact){
+            ContactManager.trigger("contact:edit", contact.get("id"));
+          });
+
+          ContactManager.regions.main.show(contactView);
+        });
       }).fail(function(response){
         console.log("Some error happened (processed in deferred's fail callback)");
         if(response.status === 404){
--- assets/js/apps/contacts/show/show_view.js
+++ assets/js/apps/contacts/show/show_view.js
@@ -3,8 +3,13 @@ ContactManager.module("ContactsApp.Show", function(Show, ContactManager, Backbon
     template: "#missing-contact-view"
   });
 
-  Show.Contact = Marionette.ItemView.extend({
+  Show.Contact = Marionette.LayoutView.extend({
     template: "#contact-view",
+
+    regions: {
+      acquaintancesRegion: "#acquaintances-region",
+      strangersRegion: "#strangers-region"
+    },
 
     events: {
       "click a.js-edit": "editClicked"
@@ -16,5 +21,31 @@ ContactManager.module("ContactsApp.Show", function(Show, ContactManager, Backbon
       e.preventDefault();
       this.trigger("contact:edit", this.model);
     }
+  });
+
+  Show.Acquaintance = Marionette.ItemView.extend({
+    tagName: "li",
+    template: "#contact-acquaintance-view",
+    triggers: {
+      "click .js-remove-acquaintance": "acquaintance:remove"
+    }
+  });
+
+  Show.Acquaintances = Marionette.CollectionView.extend({
+    tagName: "ul",
+    childView: Show.Acquaintance
+  });
+
+  Show.Stranger = Marionette.ItemView.extend({
+    tagName: "li",
+    template: "#contact-stranger-view",
+    triggers: {
+      "click .js-add-acquaintance": "acquaintance:add"
+    }
+  });
+
+  Show.Strangers = Marionette.CollectionView.extend({
+    tagName: "ul",
+    childView: Show.Stranger
   });
 });
