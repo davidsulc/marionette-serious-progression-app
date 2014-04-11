6fdc3bef3d917e9378d8a21c9b1c4f48f141a3da
David Sulc <davidsulc@gmail.com>
2014-04-11 07:51:05 +0200

    implement behaviors

--- assets/js/apps/contacts/show/show_view.js
+++ assets/js/apps/contacts/show/show_view.js
@@ -59,8 +59,11 @@ ContactManager.module("ContactsApp.Show", function(Show, ContactManager, Backbon
   Show.Acquaintance = Marionette.ItemView.extend({
     tagName: "li",
     template: "#contact-acquaintance-view",
-    triggers: {
-      "click .js-remove-acquaintance": "acquaintance:remove"
+
+    behaviors: {
+      Confirmable: {
+        event: "acquaintance:remove"
+      }
     }
   });
 
@@ -72,8 +75,14 @@ ContactManager.module("ContactsApp.Show", function(Show, ContactManager, Backbon
   Show.Stranger = Marionette.ItemView.extend({
     tagName: "li",
     template: "#contact-stranger-view",
-    triggers: {
-      "click .js-add-acquaintance": "acquaintance:add"
+
+    behaviors: {
+      Confirmable: {
+        event: "acquaintance:add",
+        message: function(view){
+          return "Add " + view.model.get("firstName") + " as an acquaintance?";
+        }
+      }
     }
   });
 
